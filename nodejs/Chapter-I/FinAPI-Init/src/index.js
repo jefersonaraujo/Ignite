a73d35fb1a221f984c9d9fd5f5b2696c2288ae1f
Jeferson Araujo <jefersonaraujo95@gmail.com>
2021-09-20 19:48:18 -0300

    init api

--- nodejs/Chapter-I/FinAPI-Init/src/index.js
+++ nodejs/Chapter-I/FinAPI-Init/src/index.js
@@ -1,7 +1,24 @@
-const { response } = require('express');
 const express = require('express');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
-app.use(express.json());
+
+app.use(express.json())
+const costomers = [];
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+
+    const id = uuidv4();
+
+    costomers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    });
+    console.log(costomers);
+    return response.status(201).send();
+});
 
 app.listen(3333)
